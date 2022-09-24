a5c9ef0e1902008034ec704e81d15be51fb34141
Ant-h0ney <krot1326@gmail.com>
2022-09-24 20:34:12 +0300

    transfer userdata

---
+++ tests/Practice form/data.py
@@ -0,0 +1,13 @@
+class User:
+    name: str = 'Anthony'
+    surname: str = 'Kononov'
+    mail: str = 'mail@mail.com'
+    gender: str = 'Female'  # Male/Female/Other
+    phone_number: str = '0123456789'  # must be 10-digit
+    birthday: dict = {'day': '28', 'month': 'March', 'year': '1995'}
+    subjects: tuple = ('Math', 'Chemistry')
+    hobbies: tuple = ('Sports', 'Reading')  # Sports/Reading/Music
+    picture: str = 'photo.jpg'
+    address: str = 'bigadress a lot of letters inside string'
+    state: str = 'Haryana'
+    city: str = 'Karnal'
--- tests/Practice form/test_practice_form.py
+++ tests/Practice form/test_practice_form.py
@@ -1,58 +1,40 @@
 import model
 from model.pages import practice_form
-
-name = 'Anthony'
-surname = 'Kononov'
-mail = 'mail@mail.com'
-gender = 'Female'  # Male/Female/Other
-phone_number = '0123456789'  # must be 10-digit
-birthday = {'day': '28', 'month': 'March', 'year': '1995'}
-subjects = ('Math', 'Chemistry')
-hobbies = ('Sports', 'Reading')  # Sports/Reading/Music
-picture = 'photo.jpg'
-address = 'bigadress a lot of letters inside string'
-state = 'Haryana'
-city = 'Karnal'
-'''
-#     [{'state': 'NCR', 'city': ['Delhi', 'Guargon', 'Noida']},
-#     {'state': 'Uttar Pradesh', 'city': ['Agra', 'Lucknow', 'Merrut']},
-#     {'state': 'Haryana', 'city': ['Karnal', 'Panipat']},
-#     {'state': 'Rajasthan', 'city': ['Jaipur', 'Jaiselmer']}]
-'''
+from data import User
 
 
 def test_fill_successful_form():
     practice_form.open_page()
-    model.general.forms.fill_name(name)
-    model.general.forms.fill_surname(surname)
-    model.general.forms.fill_email(mail)
-    practice_form.click_gender_radio(gender)
-    model.general.forms.fill_phone_number(phone_number)
-    model.general.forms.choose_birthdate_by_typing(birthday)
-    # model.general.forms.choose_birthdate_by_clicking(birthday)
-    model.general.forms.set_subjects_by_typing(subjects)
-    # model.general.forms.set_subjects_by_clicking(subjects)
-    practice_form.set_hobbies(hobbies)
-    model.general.forms.upload_picture(picture)
-    model.general.forms.fill_address(address)
-    practice_form.set_state_by_typing(state)
-    # practice_form.set_state_by_clicking(state)
-    practice_form.set_city_by_typing(city)
-    # practice_form.set_city_by_clicking(city)
+    model.general.forms.fill_name(User.name)
+    model.general.forms.fill_surname(User.surname)
+    model.general.forms.fill_email(User.mail)
+    practice_form.click_gender_radio(User.gender)
+    model.general.forms.fill_phone_number(User.phone_number)
+    model.general.forms.choose_birthdate_by_typing(User.birthday)
+    # model.general.forms.choose_birthdate_by_clicking(user.birthday)
+    model.general.forms.set_subjects_by_typing(User.subjects)
+    # model.general.forms.set_subjects_by_clicking(user.subjects)
+    practice_form.set_hobbies(User.hobbies)
+    model.general.forms.upload_picture(User.picture)
+    model.general.forms.fill_address(User.address)
+    practice_form.set_state_by_typing(User.state)
+    # practice_form.set_state_by_clicking(user.state)
+    practice_form.set_city_by_typing(User.city)
+    # practice_form.set_city_by_clicking(user.city)
     model.general.forms.click_on_submit()
 
     # Проверка формы:
     practice_form.check_the_table(
-        name,
-        surname,
-        mail,
-        gender,
-        phone_number,
-        birthday,
-        subjects,
-        hobbies,
-        picture,
-        address,
-        state,
-        city,
+        User.name,
+        User.surname,
+        User.mail,
+        User.gender,
+        User.phone_number,
+        User.birthday,
+        User.subjects,
+        User.hobbies,
+        User.picture,
+        User.address,
+        User.state,
+        User.city,
     )
