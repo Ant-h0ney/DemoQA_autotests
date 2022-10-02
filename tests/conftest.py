98a4228170f639bf820c9f07a6f51b2411583cb4
Ant-h0ney <krot1326@gmail.com>
2022-10-02 14:15:00 +0300

    Try to relocate Allure attachments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
+import allure
 import pytest
+from allure_commons.types import AttachmentType
 from selene.support.shared import browser
 
 
@@ -14,3 +16,41 @@ def browser_preparation():
 @pytest.fixture(autouse=True)
 def change_test_dir(request, monkeypatch):
     monkeypatch.chdir(request.fspath.dirname)
+    yield
+
+
+''' It deserve to exist. But must be improved (cuz a lot of lines at Allure)
+@pytest.fixture(autouse=True)
+def screenshot():
+    yield
+    screen = browser.driver.get_screenshot_as_png()
+    allure.attach(
+        screen,
+        name='screenshot',
+        attachment_type=AttachmentType.PNG,
+    )
+
+
+@pytest.fixture(autouse=True)
+def logs():
+    yield
+    logfile = ''.join(
+        f'{text}\n' for text in browser.driver.get_log(log_type='browser')
+    )
+    allure.attach(
+        logfile,
+        name='browser_log',
+        attachment_type=AttachmentType.TEXT,
+    )
+
+
+@pytest.fixture(autouse=True)
+def html():
+    yield
+    source = browser.driver.page_source
+    allure.attach(
+        source,
+        name='HTML',
+        attachment_type=AttachmentType.HTML,
+    )
+'''
