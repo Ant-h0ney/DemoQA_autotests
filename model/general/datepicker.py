a2c2aa6a564549f4ae3823bc4897a2e1b0f6329f
Ant-h0ney <krot1326@gmail.com>
2022-10-06 00:52:57 +0300

    refactor

--- model/general/datepicker.py
+++ model/general/datepicker.py
@@ -8,8 +8,8 @@ from selenium.webdriver import Keys
 @allure.step('Set birthdate {birthdate}')
 def click_birthdate(birthdate: dict):
     browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').type(birthdate['month'])
-    browser.element('.react-datepicker__year-select').type(birthdate['year'])
+    browser.element('.react-datepicker__month-select').set(birthdate['month'])
+    browser.element('.react-datepicker__year-select').set(birthdate['year'])
     browser.element(
         f'[aria-label*="{birthdate["month"]} {birthdate["day"]}"][aria-label$="{birthdate["year"]}"]'
     ).click()
@@ -18,10 +18,9 @@ def click_birthdate(birthdate: dict):
 @allure.step('Set birthdate {birthdate}')
 def type_birthdate(birthdate: dict):
     if platform.system() == 'Windows' or platform.system() == 'Linux':
-        browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL, 'a').type(
-            f'{birthdate["day"]} {birthdate["month"]} {birthdate["year"]}'
-        ).press_enter()
+        os_key = Keys.CONTROL
     else:
-        browser.element('#dateOfBirthInput').send_keys(Keys.COMMAND, 'a').type(
-            f'{birthdate["day"]} {birthdate["month"]} {birthdate["year"]}'
-        ).press_enter()
+        os_key = Keys.COMMAND
+    browser.element('#dateOfBirthInput').send_keys(os_key, 'a').type(
+        f'{birthdate["day"]} {birthdate["month"]} {birthdate["year"]}'
+    ).press_enter()
