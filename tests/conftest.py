a589360d2413cf67ec59878818a761e50098a468
Ant-h0ney <krot1326@gmail.com>
2022-10-05 14:00:52 +0300

    Littel DRY

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import platform
 
 import pytest
-from selene import Browser, Config
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,19 +12,14 @@ import model
 #     parser.addoption()
 
 
-@pytest.fixture(
-    scope='function', autouse=True
-)
-def browser_preparation_local():
+@pytest.fixture(scope='function', autouse=True)
+def browser_preparation_local(attachments):
+    browser.config.browser_name = 'chrome'
+    browser.config.window_width = 1200
+    browser.config.window_height = 1000
+    browser.config.base_url = 'https://demoqa.com'
     if platform.system() == 'Windows':
-        browser.config.browser_name = 'chrome'
-        browser.config.window_width = 1200
-        browser.config.window_height = 1000
-        browser.config.base_url = 'https://demoqa.com'
         yield
-        model.utils.allure.attach.screenshot()
-        model.utils.allure.attach.logs()
-        model.utils.allure.attach.html()
     else:
         options = Options()
         selenoid_capabilities = {
@@ -39,17 +33,7 @@ def browser_preparation_local():
             options=options,
         )
         browser.config.driver = driver
-        browser.config.browser_name = 'chrome'
-        browser.config.window_width = 1200
-        browser.config.window_height = 1000
-        browser.config.base_url = 'https://demoqa.com'
         yield
-        model.utils.allure.attach.screenshot()
-        model.utils.allure.attach.logs()
-        model.utils.allure.attach.html()
-        model.utils.allure.attach.video()
-
-    browser.quit()
 
 
 @pytest.fixture(autouse=True)
@@ -58,40 +42,12 @@ def change_test_dir(request, monkeypatch):
     yield
 
 
-'''
 @pytest.fixture(scope='function')
-def browser_preparation_selenoid():
-    options = Options()
-    selenoid_capabilities = {
-        'browserName': 'chrome',
-        'browserVersion': '100.0',
-        'selenoid:options': {'enableVNC': True, 'enableVideo': True},
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor='https://user1:1234@selenoid.autotests.cloud/wd/hub',
-        options=options,
-    )
-    # browser = Browser(Config(driver))
-    browser.config.driver = driver
-    browser.config.browser_name = 'chrome'
-    browser.config.window_width = 1200
-    browser.config.window_height = 1000
-    browser.config.base_url = 'https://demoqa.com'
+def attachments():
     yield
     model.utils.allure.attach.screenshot()
     model.utils.allure.attach.logs()
     model.utils.allure.attach.html()
-    model.utils.allure.attach.video()
-
-    browser.quit()'''
-
-# @pytest.fixture(scope='function')
-# def attachments():
-#     yield
-#     model.utils.allure.attach.screenshot()
-#     model.utils.allure.attach.logs()
-#     model.utils.allure.attach.html()
-#     selenoid_driver = webdriver.Remote()
-#     if browser.config.driver == selenoid_driver:
-#         model.utils.allure.attach.video()
+    if platform.system() != 'Windows':
+        model.utils.allure.attach.video()
+    browser.quit()
